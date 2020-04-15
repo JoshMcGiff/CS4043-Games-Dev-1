5989bfd5d81b5526aec7f1b9d6bc230f9e43005a
Slattz <Slattz@users.noreply.github.com>
2020-04-15 11:41:50 +0100

    add going to mainmenu line, '''fixing''' it

--- main.lua
+++ main.lua
@@ -9,4 +9,6 @@
 	--native.setProperty("mouseCursorVisible", false) <- Enable this when Shane implements a custom cursor because mouse is ugly
 	--native.setProperty("windowMode", "fullscreen") -- puts app in fullscreen mode when built, pause menu needed before enabling
 	
+	--swap these two lines for when wanting to go into menu first and straight into game
 	composer.gotoScene("Game",{ time=800, effect="crossFade" })
+	--composer.gotoScene("MainMenu",{ time=800, effect="crossFade" }) 
